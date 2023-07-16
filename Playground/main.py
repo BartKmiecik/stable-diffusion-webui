a9ff5b5795d430fc75a43c9a1ddbbe845b9c2e82
Bart <kmiecikjob@gmail.com>
2023-07-16 13:14:11 +0200

    Adding some mask, doing research how to use inpaint by api

--- Playground/main.py
+++ Playground/main.py
@@ -1,1 +1,20 @@
+import PIL
+from IPython.display import display
+from PIL import Image
 import requests
+import torch
+from io import BytesIO
+
+from diffusers import StableDiffusionInpaintPipeline
+
+# img_url = Image.open("Test4.png")
+# mask_url = Image.open("Mask0a.png")
+#
+# init_image = img_url.resize((512, 512))
+# mask_image = mask_url.resize((512, 512))
+#
+#
+# image.save('test.png')
+
+request = requests.get('http://127.0.0.1:7860/sdapi/v1/options')
+print(request.text)
