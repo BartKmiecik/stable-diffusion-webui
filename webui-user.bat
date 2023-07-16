cce7a3f8f225be3b5ea59524757fcf135364704a
Bart <kmiecikjob@gmail.com>
2023-07-16 12:47:54 +0200

    Creating playgroundChannging it to work with api

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON= C:\Users\user\AppData\Local\Programs\Python\Python311\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--api --listen
 
 call webui.bat
